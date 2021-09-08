f96ec464db5e19b0b8e88d7512aa78231a2ac160
h-tess <62144074+h-tess@users.noreply.github.com>
2021-09-08 16:55:34 -0400

    [update] kNN & NaiveBayes files

--- Assignment1/NaiveBayes.py
+++ Assignment1/NaiveBayes.py
@@ -2,8 +2,13 @@ def preprocessor():
     pass
 
 def classify():
-    pass
+    return prediction
+
 
 def run(training_set, test_set):
-    pass
+    prob_dictionnary = preprocessor(training_set)
+    test_set, t_labels= preprocessor(test_set)
+    
+    p_labels = [classify(new_data, prob_dictionnary) for new_data in test_set]
 
+    return p_labels, t_labels
--- Assignment1/kNN.py
+++ Assignment1/kNN.py
@@ -11,8 +11,12 @@ def classify(feature_vector, training_labels, new_data):
 
 def run(training_set, test_set):
     feature_vector = preprocessor(training_set)
+    p_label = []
+    t_label = []
     for i in test_set:
         p_label.append(classify(feature_vector, i))
         t_label.append()
         # FP? TP? FN? TN?
+
+    return( p_label, t_label)
 
