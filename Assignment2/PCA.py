f46b599df8f6a0b7e8bde12cf09aba81bdb938c6
h-tess <62144074+h-tess@users.noreply.github.com>
2021-09-22 16:35:02 -0400

    [update] PCA functions documentation

--- Assignment2/PCA.py
+++ Assignment2/PCA.py
@@ -3,16 +3,18 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-def tune_number_of_components():
+
+def tune_number_of_components(dataset):
     """
+    ## Plot the explained variance ratio as a function of the number of components
 
+        `parameters`
+            dataset: data on which to perform PCA
 
-
-
-
-    """
+        `return`
+            plt: plots of the explained variance ratio and cumulative explained variance ratio
     
-    dataset = np.load("data/training_normal.npy")
+    """
 
     n_features = dataset.shape[1]
 
@@ -36,15 +38,22 @@ def tune_number_of_components():
     ax2.bar(np.arange(n_features), np.cumsum(pca.explained_variance_ratio_)*100)
     plt.xlabel("number of components"); plt.ylabel("cumulative explained variance ratio (%)")
     plt.show()
+    
+    return plt
 
 
 def reduce_dimensions(training, testing, n_components = 5):
     """
+    ## Reduce the number of features by applying PCA
 
+        `parameters`
+            training: training set on which to compute new components
+            testing: testing set on which to apply new components
 
-
-
-
+        `return`
+            projected_training: transformed training set with the new components
+            projected_testing: transformed testing set with the new components
+    
     """
     
     # ---- Fit PCA components on training set
@@ -58,20 +67,21 @@ def reduce_dimensions(training, testing, n_components = 5):
     return projected_training, projected_testing
 
 
-def compare2D():
+def compare2D(training, testing_normal, testing_attack):
+    """
+    ## Comparison of the testing normal and testing attack datasets in the 2 first dimensions of the PCA feature space
+
+        `parameters`
+            training: training set on which to compute the two components
+            testing_normal: testing normal set to project on those two components
+            testing_attack: testing attack set to project on those two components
+
+        `return`
+            plt: Projections of the testing_normal and testing_attack sets colored with their labels
+
     """
 
-
-
-
-
-    """
-    # ---- Import data
-    training = np.load("data/training_normal.npy")
-    testing_normal = np.load("data/testing_normal.npy")
-    testing_attack = np.load("data/testing_attack.npy")
-
-    # ---- Fit PCA on train set
+    # ---- Fit PCA on training set
     pca = PCA(n_components=2)  
     pca.fit(training)  
 
@@ -89,21 +99,24 @@ def compare2D():
     plt.legend()
     plt.show()
 
+    return plt
 
-def compare3D():
+
+def compare3D(training, testing_normal, testing_attack):
+    """
+    ## Comparison of the testing normal and testing attack datasets in the 3 first dimensions of the PCA feature space
+
+        `parameters`
+            training: training set on which to compute the three components
+            testing_normal: testing normal set to project on those three components
+            testing_attack: testing attack set to project on those three components
+
+        `return`
+            plt: Projections of the testing_normal and testing_attack sets colored with their labels
+
     """
 
-
-
-
-
-    """
-    # ---- Import data
-    training = np.load("data/training_normal.npy")
-    testing_normal = np.load("data/testing_normal.npy")
-    testing_attack = np.load("data/testing_attack.npy")
-
-    # ---- Fit PCA on train set
+    # ---- Fit PCA on training set
     pca = PCA(n_components=3)  
     pca.fit(training)
 
@@ -123,12 +136,20 @@ def compare3D():
     plt.legend()
     plt.show()
 
+    return plt
+
+
 
 if __name__ == "__main__":
 
+    # ---- Load data
+    training = np.load("data/training_normal.npy")
+    testing_normal = np.load("data/testing_normal.npy")
+    testing_attack = np.load("data/testing_attack.npy")
+
     # ---- Comparison of the normal/attack in the projected space
-    compare2D()
-    # compare3D()
+    compare2D(training, testing_normal, testing_attack)
+    # compare3D(training, testing_normal, testing_attack)
 
     # ---- Choose the best number of features for the dimension reduction
-    # tune_number_of_components()
+    # tune_number_of_components(training)
