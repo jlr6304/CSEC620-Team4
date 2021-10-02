601ebb294403c24b4b1d6bbbd3cd02dc6a16d29e
h-tess <62144074+h-tess@users.noreply.github.com>
2021-10-02 11:59:17 -0400

    [update] add performance evaluation for DBSCAN

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -5,25 +5,33 @@ from functions import get_data, score
 import time
 
 def main():
-    # load and preprocess data
+    # ----- load and preprocess data
     training, testing, labels = get_data(n_testing_samples=4000, attacks_ratio = .2)
     condensed_training, condensed_testing = PCA.reduce_dimensions(training, testing, 10)
-    
-    # DBSCAN anomaly detection
+
+    bold = lambda x: '\033[1m' + x + '\033[0m'
+
+    # ----- DBSCAN anomaly detection
+    print(bold("DBSCAN anomaly detection"))
     DBSCAN_start = time.time()
-    DBSCAN_labels = DBSCAN.run(training, testing, min_neighbors, epsilon)
+    DBSCAN_labels = DBSCAN.run(training, testing, min_neighbors=10, epsilon=.015, verbose=False)
     DBSCAN_end = time.time()
     
-    # Kmeans anomaly detection
-    Kmeans_start = time.time()
-    Kmeans_labels = Kmeans.run(training, testing, k, t)
-    Kmeans_end = time.time()
+    # ----- Kmeans anomaly detection
+    # print(bold("k-Means anomaly detection"))
+    # Kmeans_start = time.time()
+    # Kmeans_labels = Kmeans.run(training, testing, k, t)
+    # Kmeans_end = time.time()
     
-    # Performances comparison
-    print(DBSCAN_end-DBSCAN_start)
-    print(Kmeans_end-Kmeans_start)
-    score(DBSCAN_labels, labels)
-    score(Kmeans_labels, labels)
+    # ----- Performances comparison
+    # DBSCAN
+    print(bold("DBSCAN performances"))
+    print("Execution time:", DBSCAN_end - DBSCAN_start)
+    score(DBSCAN_labels, labels, ["confusion_mat", "accuracy", "TPR", "recall", "FPR", "F1score"])
+    # k-Means
+    print(bold("k-Means performances"))
+    print("Execution time:", Kmeans_end - Kmeans_start)
+    score(Kmeans_labels, labels, ["confusion_mat", "accuracy", "TPR", "recall", "FPR", "F1score"])
 
 
 if __name__ == "__main__":
--- Assignment2/DBSCAN.py
+++ Assignment2/DBSCAN.py
@@ -4,44 +4,60 @@ import time
 import sklearn
 
 
-
-def find_points_in_epilsion(distances_of_points, epsilon, point_position):
+def find_points_in_epsilon(distances_of_points, epsilon, point_position):
     points_in_epilion = []
     for new_point_position in range(0, len(distances_of_points)):
         if (distances_of_points[point_position][new_point_position] < epsilon):
             points_in_epilion.append(new_point_position)
     return points_in_epilion
 
-def determine_core_points(distances_of_points,epsilon,min_neighbors):
+
+def determine_core_points(distances_of_points,epsilon,min_neighbors, verbose=False):
+    print("\tdetermining core points in training set")
+    
     core_points_positions=[]
     for point_position in range(0,len(distances_of_points)):
-        if len(find_points_in_epilsion(distances_of_points, epsilon,point_position))>=min_neighbors:
-            print("core point found")
+        if len(find_points_in_epsilon(distances_of_points, epsilon,point_position))>=min_neighbors:
+            if verbose:
+                print("core point found")
             core_points_positions.append(point_position)
     return core_points_positions
 
-def test(testing, training, core_points,epsilon):
-    print(sklearn.metrics.pairwise.euclidean_distances(testing, core_points))
+
+def test(testing, training, core_points, epsilon, verbose = False):
+    print("\tanomaly detection on testing set")
+    
     distances_from_core_points=sklearn.metrics.pairwise.euclidean_distances(testing, core_points)
+    # print(distances_from_core_points) # print the distance with core points
+
+    labels = []
     for sample in distances_from_core_points:
         anomaly=True
         for distance in sample:
             if distance <=epsilon:
                 anomaly=False
-        if anomaly==True:
-            print("anomaly")
-        if anomaly==False:
-            print("not anomaly")
+                break
+        labels.append(1 if anomaly else 0)
+        
+        # Print the category
+        if verbose:
+            if anomaly==True:
+                print("anomaly")
+            if anomaly==False:
+                print("not anomaly")
+
+    return labels
 
 
+def run(training, testing, min_neighbors, epsilon, verbose = False):
 
-
-def run(training, testing, min_neighbors, epsilon):
     distances_of_points=sklearn.metrics.pairwise.euclidean_distances(training,training)
-    print(distances_of_points)
-    core_point_positions=determine_core_points(distances_of_points,epsilon,min_neighbors)
+    # print(distances_of_points) # print the distance between training points
+    
+    core_point_positions=determine_core_points(distances_of_points,epsilon,min_neighbors, verbose)
     core_points=[]
+    
     for position in core_point_positions:
         core_points.append(training[position])
-    test(testing, training, core_points,epsilon)
-
+    
+    return test(testing, training, core_points, epsilon, verbose)
