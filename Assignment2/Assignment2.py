036cf514fdf15506ee8a6f4582c70d1bf4aee4d2
h-tess <62144074+h-tess@users.noreply.github.com>
2021-10-04 23:12:10 -0400

    [update] assignment2.py hyperparameters
    Set hyperparameters to their best values

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -7,27 +7,28 @@ import time
 def main():
     # ----- load and preprocess data
     training, testing, labels = get_data(n_testing_samples=4000, attacks_ratio = .2)
-    condensed_training, condensed_testing = PCA.reduce_dimensions(training, testing, 10)
+    condensed_training, condensed_testing = PCA.reduce_dimensions(training, testing, 12)
 
     bold = lambda x: '\033[1m' + x + '\033[0m'
 
     # ----- DBSCAN anomaly detection
     print(bold("DBSCAN anomaly detection"))
     DBSCAN_start = time.time()
-    DBSCAN_labels = DBSCAN.run(training, testing, min_neighbors=10, epsilon=.015, verbose=False)
+    DBSCAN_labels = DBSCAN.run(condensed_training, condensed_testing, min_neighbors=10, epsilon=.03, verbose=False)
     DBSCAN_end = time.time()
     
     # ----- Kmeans anomaly detection
-    # print(bold("k-Means anomaly detection"))
-    # Kmeans_start = time.time()
-    # Kmeans_labels = Kmeans.run(training, testing, k, t)
-    # Kmeans_end = time.time()
+    print(bold("k-Means anomaly detection"))
+    Kmeans_start = time.time()
+    Kmeans_labels = Kmeans.run(condensed_training, condensed_testing, k=8, t=.18, l=.01)
+    Kmeans_end = time.time()
     
     # ----- Performances comparison
     # DBSCAN
     print(bold("DBSCAN performances"))
     print("Execution time:", DBSCAN_end - DBSCAN_start)
     score(DBSCAN_labels, labels, ["confusion_mat", "accuracy", "TPR", "recall", "FPR", "F1score"])
+    
     # k-Means
     print(bold("k-Means performances"))
     print("Execution time:", Kmeans_end - Kmeans_start)
